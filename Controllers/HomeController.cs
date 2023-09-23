00fbc6a13a3005209c55eb9dc4e974d95bc07927
p <p@DESKTOP-UBUDTBS>
2023-09-23 13:02:46 +0200

    umbraco live with 2 pages

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace cms_assignment.Controllers
+{
+    public class HomeController : UmbracoController
+    {
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+    }
+}
